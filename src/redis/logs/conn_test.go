5b6c01816dfa557dfa4109e61875021e9a17198e
liwenhua <2685511234@qq.com>
2018-08-12 21:37:39 +0800

    add license

--- src/redis/logs/conn_test.go
+++ src/redis/logs/conn_test.go
@@ -22,3 +22,4 @@ func TestConn(t *testing.T) {
 	log := NewLogger(1000)
 	log.SetLogger("conn", `{"net":"tcp","addr":":7020"}`)
 	log.Informational("informational")
+}
--- src/redis/server/utils.go
+++ src/redis/server/utils.go
@@ -1,8 +1,27 @@
+// Copyright 2018 liwenhua. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 package server
 
 import (
 	"fmt"
 	"strconv"
+	"github.com/Unknwon/goconfig"
+	"strings"
+	"os"
+	"errors"
 )
 
 func MustString(any interface{}) string {
@@ -35,4 +54,46 @@ func InArray(s string, arr []string) bool {
 		}
 	}
 	return false
+}
+
+func FileExist(filename string) bool {
+	if filename == "" {
+		return false
+	}
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+
+func ReadMapFromIni(confpath string, args ...string) (arrMap map[string]string, err error) {
+	if !FileExist(confpath) {
+		arrMap = make(map[string]string)
+		err = errors.New("file not exists. path:" + confpath)
+		panic(err)
+	}
+
+	ini, err := goconfig.LoadConfigFile(confpath)
+	if err != nil {
+		panic(err)
+	}
+
+	keylist := ini.GetKeyList("")
+	arrMap = make(map[string]string, len(keylist))
+
+	keyprefix := ""
+	if len(args) > 0 {
+		keyprefix = strings.TrimSpace(args[0])
+	}
+
+	if keyprefix == "" {
+		for _, key := range keylist {
+			arrMap[key], _ = ini.GetValue("", key)
+		}
+	} else {
+		for _, key := range keylist {
+			arrMap[strings.TrimPrefix(key, keyprefix)], _ = ini.GetValue("", key)
+		}
+	}
+
+	return
 }
